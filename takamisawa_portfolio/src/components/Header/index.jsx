02ed73d15b342dbce114ca6b27be3748395671e2
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-01-07 15:53:18 -0300

    adicionando header

--- takamisawa_portfolio/src/components/Header/index.jsx
+++ takamisawa_portfolio/src/components/Header/index.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Container } from "./style";
+import { Container, Navigation, Logo, Wrapper, Socials } from "./style";
+import Linkedin from "../../assets/lkdLogo.png";
+import Instagram from "../../assets/instaLogo.png";
 
 export function Header() {
     return (
         <Container>
-            <header>
-                <nav>
-                    <div>
-                        <h1>Yuri Takamisawa </h1>
-                    </div>
-                    <ul>
-                        <li><a href="/">Projetos</a></li>
-                        <li><a href="/">Sobre</a></li>
-                        
-                    </ul>
-                </nav>
-            </header>
+            <Wrapper>
+                    <Logo>
+                        <h1><a href="/"> Yuri Takamisawa </a></h1>
+                    </Logo>
+                    <Navigation>
+                        <ul>
+                            <li><a href="/">Projetos</a></li>
+                            <li><a href="/">Sobre</a></li>
+
+                        </ul>
+                    </Navigation>
+                    <Socials>
+                        <ul>
+                            <li><a href="/"><img src={Linkedin} alt="Linkedin" /></a></li>
+                            <li><a href="/"><img src={Instagram} alt="Instagram" /></a></li>
+                        </ul>
+                    </Socials>
+            </Wrapper>
         </Container>
-        
+
     )
 }
--- takamisawa_portfolio/src/components/Header/style.js
+++ takamisawa_portfolio/src/components/Header/style.js
@@ -1,3 +1,87 @@
 import styled from "styled-components";
 
-export const Container = styled.header``;
+export const Container = styled.header`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    overflow-x: hidden;
+    overflow-y: auto;
+    color: #121212;
+    
+    & > div {
+        grid-column: 1 / 2; 
+        background-color: #F5F5F5;
+    }
+`;
+
+export const Wrapper = styled.div`
+    height: auto;
+    padding-top: 65px;
+    padding-left: 35px;
+    padding-bottom:25px;
+    display: grid;
+    grid-template-rows: auto 1fr auto; // divide o Wrapper em três linhas: topo, meio e fundo
+`;
+
+
+
+export const Logo  = styled.div`
+h1 {
+        text-align: left; 
+        padding-bottom:25px;
+    }
+    
+    a {
+        
+        text-decoration: none;
+        color: inherit; 
+    }
+
+`;
+
+export const Navigation = styled.nav`
+    ul {
+        list-style: none;
+        text-align: left;
+        li {
+            margin-bottom:13px;
+            
+            a {
+                color: inherit; 
+                text-decoration: none; 
+                transition: color 0.3s ease; 
+                letter-spacing: .12px; 
+                line-height: 1.25em; 
+                font-size: 20px;
+                &:hover {
+                    color: rgba(0,0,0,.4); 
+                }
+            }
+        }
+    }
+`;
+
+export const Socials = styled.nav`
+align-self: end;
+ul {
+        list-style: none; 
+        display:flex;
+        gap:8px;
+        li {
+            a {
+                img {
+                    width: 35px; 
+                    height: 35px; 
+                    transition: opacity 0.3s ease;
+                    &:hover {
+                        opacity: 0.6;
+                    }
+                }
+            }
+        }
+    }
+`;
